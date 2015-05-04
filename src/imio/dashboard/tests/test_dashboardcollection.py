f510b0bcf5231489462b49baf780c93f3a8a727d
Gauthier Bastien <g.bastien@imio.be>
2015-05-04 14:56:00 +0200

    Added testing for DashboardCollection

---
+++ src/imio/dashboard/tests/test_dashboardcollection.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""Setup/installation tests for this package."""
+
+from plone import api
+from imio.dashboard.testing import IntegrationTestCase
+from imio.dashboard.interfaces import ICustomViewFieldsVocabulary
+
+
+class TestDashboardCollection(IntegrationTestCase):
+    """Test the DashboardCollection content type."""
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        self.dashboardcollection = api.content.create(
+            id='dc1',
+            type='DashboardCollection',
+            title='Dashboard collection 1',
+            container=self.portal
+        )
+
+    def test_listMetaDataFields(self):
+        """Test the overrided listMetaDataFields method that will
+           list available metadata as base Collection but that will
+           also add to it's vocabulary, elements returned by an
+           CustomViewFieldsVocabularyAdapter adapter."""
+        # classic metadata
+        metadatas = self.portal.portal_atct.getMetadataDisplay(True).keys()
+        # additional fields
+        additionalFields = ['actions', ]
+        # additional fields from adapter
+        self.assertTrue(ICustomViewFieldsVocabulary(self.dashboardcollection).additionalViewFields().keys() ==
+                        additionalFields)
+        # complete list from adapter
+        self.assertTrue(set(ICustomViewFieldsVocabulary(self.dashboardcollection).listMetaDataFields().keys()) ==
+                        set(metadatas + additionalFields))
+        # listMetaDataFields is now using adapter
+        self.assertTrue(set(self.dashboardcollection.listMetaDataFields().keys()) ==
+                        set(metadatas + additionalFields))
+
+    def test_selectedViewFields(self):
+        """Test the overrided selectedViewFields method that will make sure
+           additional values selected in the customViewFields field will be
+           purged of every columns that are not metadata (added by the override listMetaDataFields)
+           or it breaks the tabular_view."""
+        # select the 'actions' value in the customViewFields, this is not a metadata
+        self.dashboardcollection.setCustomViewFields(['Title', 'Creator', 'actions'])
+        # selectedViewFields returns [('Title', u'Title'), ('Creator', 'Creator')]
+        self.assertTrue([elt[0] for elt in self.dashboardcollection.selectedViewFields()] == ['Title', 'Creator', ])
