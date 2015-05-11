56b505c5fcd5a198c01cc80a9ff12e6a7b8934a3
Gauthier Bastien <g.bastien@imio.be>
2015-05-11 10:35:09 +0200

    Moved code from c.eeafaceted.z3ctable, to get view fields on the z3ctablefrom the selected collection in the collection widget

--- src/imio/dashboard/browser/overrides.py
+++ src/imio/dashboard/browser/overrides.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from collective.eeafaceted.collectionwidget.widgets.widget import CollectionWidget
 from collective.eeafaceted.z3ctable.browser.views import FacetedTableView
+from Products.CMFCore.utils import getToolByName
+
 from imio.dashboard.columns import ActionsColumn
 
 
@@ -14,3 +17,22 @@ class IDFacetedTableView(FacetedTableView):
                 column.view_name = 'actions_panel'
                 column.params = {'showHistory': True, 'showActions': False}
         return column
+
+    def _getViewFields(self):
+        """Returns fields we want to show in the table."""
+        colNames = ['Title', 'CreationDate', 'Creator', 'review_state', 'getText']
+        # if we can get the collection we are working with,
+        # use customViewFields defined on it if any
+        for criterion in self.criteria.values():
+            if criterion.widget == CollectionWidget.widget_type:
+                # value is stored in the request with ending [], like 'c4[]'
+                collectionUID = self.request.get('{0}[]'.format(criterion.getId()))
+                catalog = getToolByName(self.context, 'portal_catalog')
+                collection = catalog(UID=collectionUID)
+                if collection:
+                    collection = collection[0].getObject()
+                    customViewFields = collection.getCustomViewFields()
+                    if customViewFields:
+                        colNames = customViewFields
+        return colNames
+
