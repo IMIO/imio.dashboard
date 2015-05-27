8f55895f9bcbbc8d8231e5d4c04b91a0fd3c926d
Gauthier Bastien <g.bastien@imio.be>
2015-05-27 17:27:08 +0200

    Works also if current context is a ICollection

--- src/imio/dashboard/browser/overrides.py
+++ src/imio/dashboard/browser/overrides.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
+from plone.app.collection.interfaces import ICollection
 
 from collective.eeafaceted.collectionwidget.widgets.widget import CollectionWidget
 from collective.eeafaceted.z3ctable.browser.views import FacetedTableView
@@ -25,17 +26,24 @@ class IDFacetedTableView(FacetedTableView):
     def _getViewFields(self):
         """Returns fields we want to show in the table."""
         colNames = ['Title', 'CreationDate', 'Creator', 'review_state', 'getText']
-        # if we can get the collection we are working with,
-        # use customViewFields defined on it if any
-        for criterion in self.criteria.values():
-            if criterion.widget == CollectionWidget.widget_type:
-                # value is stored in the request with ending [], like 'c4[]'
-                collectionUID = self.request.get('{0}[]'.format(criterion.getId()))
-                catalog = getToolByName(self.context, 'portal_catalog')
-                collection = catalog(UID=collectionUID)
-                if collection:
-                    collection = collection[0].getObject()
-                    customViewFields = collection.getCustomViewFields()
-                    if customViewFields:
-                        colNames = customViewFields
+        # if the context is a collection, get customViewFields on it
+        collection = None
+        if ICollection.providedBy(self.context):
+            collection = self.context
+        else:
+            # if we can get the collection we are working with,
+            # use customViewFields defined on it if any
+            for criterion in self.criteria.values():
+                if criterion.widget == CollectionWidget.widget_type:
+                    # value is stored in the request with ending [], like 'c4[]'
+                    collectionUID = self.request.get('{0}[]'.format(criterion.getId()))
+                    catalog = getToolByName(self.context, 'portal_catalog')
+                    collection = catalog(UID=collectionUID)
+                    if collection:
+                        collection = collection[0].getObject()
+        if collection:
+            customViewFields = collection.getCustomViewFields()
+            if customViewFields:
+                colNames = customViewFields
+
         return colNames
