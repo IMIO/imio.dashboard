1519206e4f5390aaaf0adfc42b5cd1a9fc41cdeb
Stephan Geulette <s.geulette@imio.be>
2015-08-19 09:49:51 +0200

    Added helper method to enable faceted dashboard on an object

--- src/imio/dashboard/utils.py
+++ src/imio/dashboard/utils.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
+import logging
+from os import path
+from zope.interface import noLongerProvides
 
 from collective.eeafaceted.collectionwidget.widgets.widget import CollectionWidget
 
 from eea.facetednavigation.criteria.interfaces import ICriteria
 from eea.facetednavigation.subtypes.interfaces import IFacetedNavigable
+from eea.facetednavigation.interfaces import IHidePloneLeftColumn
+from eea.facetednavigation.layout.interfaces import IFacetedLayout
 
 from imio.dashboard.interfaces import NoFacetedViewDefinedException
+
+logger = logging.getLogger('imio.dashboard: utils')
 
 
 def _updateDefaultCollectionFor(folderObj, default_uid):
@@ -42,3 +49,24 @@ def getCollectionLinkWidget(faceted_context):
     """
     criterion = getCollectionLinkCriterion(faceted_context)
     return criterion.widget
+
+
+def enableFacetedDashboardFor(obj, xmlpath=None):
+    """
+    Enable a faceted view on obj and import a specific xml
+    """
+    obj.unrestrictedTraverse('@@faceted_subtyper').enable()
+    # use correct layout in the faceted
+    IFacetedLayout(obj).update_layout('faceted-table-items')
+    # show the left portlets
+    if IHidePloneLeftColumn.providedBy(obj):
+        noLongerProvides(obj, IHidePloneLeftColumn)
+    # import configuration
+    if xmlpath is not None:
+        if path.exists(xmlpath):
+            obj.unrestrictedTraverse('@@faceted_exportimport').import_xml(
+                import_file=open(xmlpath))
+        else:
+            logger.error("Specified xml file 'xmlpath' doesn't exist" % xmlpath)
+    obj.reindexObject()
+
