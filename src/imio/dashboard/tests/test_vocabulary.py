f257fc41bdb9c65364c4d956570c54eef54cd0ea
Gauthier Bastien <g.bastien@imio.be>
2015-10-01 11:21:35 +0200

    Added test for collectionsvocabulary and completed test for creatorsvocabulary

--- src/imio/dashboard/tests/test_vocabulary.py
+++ src/imio/dashboard/tests/test_vocabulary.py
@@ -50,13 +50,28 @@ class TestConditionAwareVocabulary(IntegrationTestCase):
         """This will return every users that created a content in the portal."""
         factory = queryUtility(IVocabularyFactory, u'imio.dashboard.creatorsvocabulary')
         self.assertEquals(len(factory(self.portal)), 1)
-        self.assertTrue("test_user_1_" in factory(self.portal))
+        self.assertTrue('test_user_1_' in factory(self.portal))
+        # no fullname, title is the login
+        self.assertEquals(factory(self.portal).getTerm('test_user_1_').title, 'test_user_1_')
         # add another user, create content and test again
         membershipTool = getToolByName(self.portal, 'portal_membership')
         membershipTool.addMember('test_user_2_', 'password', ['Manager'], [])
+        user2 = membershipTool.getMemberById('test_user_2_')
+        user2.setMemberProperties({'fullname': 'User 2'})
+        self.assertEquals(user2.getProperty('fullname'), 'User 2')
         login(self.portal, 'test_user_2_')
         # vocabulary cache not cleaned
         self.assertEquals(len(factory(self.portal)), 1)
         self.portal.invokeFactory('Folder', id='folder2')
         # vocabulary cache cleaned
         self.assertEquals(len(factory(self.portal)), 2)
+        self.assertEquals(factory(self.portal).getTerm('test_user_2_').title, 'User 2')
+
+    def test_collectionsvocabulary(self):
+        """This will return every DashboardCollections of the portal."""
+        factory = queryUtility(IVocabularyFactory, u'imio.dashboard.collectionsvocabulary')
+        # one DashboardCollection
+        self.assertEquals(len(factory(self.portal)), 1)
+        term = factory(self.portal).getTerm(self.dashboardcollection.UID())
+        self.assertEquals(term.token, term.value, self.dashboardcollection.UID())
+        self.assertEquals(term.title, self.dashboardcollection.Title())
