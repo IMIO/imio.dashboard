f76997b06c89bb3e5fce4c09da380c5cefce7ee0
Gauthier Bastien <g.bastien@imio.be>
2015-11-10 16:31:12 +0100

    showActions=True by default

--- src/imio/dashboard/columns.py
+++ src/imio/dashboard/columns.py
@@ -30,4 +30,4 @@ class ActionsColumn(BrowserViewCallColumn):
     header_js = '<script type="text/javascript">jQuery(document).ready(initializeOverlays);' \
                 'jQuery(document).ready(preventDefaultClickTransition);</script>'
     view_name = 'actions_panel'
-    params = {'showHistory': True, 'showActions': False}
+    params = {'showHistory': True, 'showActions': True}
