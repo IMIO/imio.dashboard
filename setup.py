fab9bd84d962fa20aef34633401a0d80fa14f641
Gauthier Bastien <g.bastien@imio.be>
2018-05-03 14:16:19 +0200

    Back to development: 1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='imio.dashboard',
-    version='1.6',
+    version='1.7.dev0',
     description="This package is the glue between different packages "
                 "offering a usable and integrated dashboard application",
     long_description=long_description,
