507be6aee3650347645a0de7f2db01f45bc4088a
Gauthier Bastien <g.bastien@imio.be>
2018-06-12 12:54:53 +0200

    Continue to move to c.e.dashboard

--- src/imio/dashboard/config.py
+++ src/imio/dashboard/config.py
@@ -1,9 +1,4 @@
 PROJECTNAME = 'imio.dashboard'
 
-ADD_PERMISSIONS = {
-    'DashboardCollection': 'imio.dashboard: Add DashboardCollection',
-}
-
-DEFAULT_PORTLET_TITLE = u'Collections'
-
 COMBINED_INDEX_PREFIX = 'combined__'
+
--- src/imio/dashboard/setuphandlers.py
+++ src/imio/dashboard/setuphandlers.py
@@ -12,6 +12,3 @@ def post_install(context):
     if isNotCurrentProfile(context):
         return
 
-    # we need to apply the ITALConditionable extender on existing elements
-    portal = context.getSite()
-    applyExtender(portal, ('DashboardCollection', ))
