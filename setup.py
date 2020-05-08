aecbc587d987ac86a40075074b6dbca20048a55b
Gauthier Bastien <g.bastien@imio.be>
2020-05-08 16:46:16 +0200

    Back to development: 2.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (
 
 setup(
     name='imio.dashboard',
-    version='2.7',
+    version='2.8.dev0',
     description="This package adds functionnality to collective.eeafaceted.dashboard "
                 "but only work for Plone 4.3.x",
     long_description=long_description,
