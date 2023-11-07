c43187a4682e600a88171d069be887d013a12a79
Martin Peeters <martin.peeters@affinitic.be>
2023-11-07 11:25:03 +0100

    Backup local changes made directly on server

--- src/imio/dashboard/browser/static/imio.dashboard.js
+++ src/imio/dashboard/browser/static/imio.dashboard.js
@@ -33,7 +33,7 @@ function generatePodDocument(template_uid, output_format, tag) {
 
 $(document).ready(function () {
   var url = $('base').attr('href') + '/@@json_collections_count';
-  if ($('.faceted-tagscloud-collection-widget').length > 0) {
+  if (($('base').attr('href').match('schedule')) && $('.faceted-tagscloud-collection-widget').length > 0) {
     $(Faceted.Events).bind(Faceted.Events.AJAX_QUERY_SUCCESS, function() {
         $.get(url, function (response) {
             var info = JSON.parse(response);
